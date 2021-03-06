42841117782112d424191a688b86827ef983b5c9
mattnj58 <mattnj58@gmail.com>
2021-03-06 11:02:28 -0500

    i think i fixed the automation

--- bot.py
+++ bot.py
@@ -64,24 +64,24 @@ person = " "
 
 @bot.command(brief="This is a list of commands that are currently planned/in production")
 async def todo(ctx, op, command):
-	if ctx.message.author.id == 173502986448797696:
-		if op == "add" || op =="a":
-			try: 
-				f = open("todo.txt", mode="a")
-				f.write(command + "\n")
-				f.close
-			except:
-				print("Something went wrong with adding, it'll be fixed")
-		elif op == "remove" || op == "r":
-			try:
-				f=open("todo.txt", mode="w+")
-				data = f.read()
-				data = data.replace(command,"")
-				f.write(data)
-			except:
-				print("Something went wrong with removing, it'll be fixed soon")
-	else:
-		await ctx.send("You do not own this bot!")
+	# if ctx.message.author.id == 173502986448797696:
+	# 	if op == "add" or op =="a":
+	# 		try: 
+	# 			f = open("todo.txt", mode="a")
+	# 			f.write(command + "\n")
+	# 			f.close
+	# 		except:
+	# 			print("Something went wrong with adding, it'll be fixed")
+	# 	elif op == "remove" or op == "r":
+	# 		try:
+	# 			f=open("todo.txt", mode="w+")
+	# 			data = f.read()
+	# 			data = data.replace(command,"")
+	# 			f.write(data)
+	# 		except:
+	# 			print("Something went wrong with removing, it'll be fixed soon")
+	# else:
+	# 	await ctx.send("You do not own this bot!")
 	user = str(ctx.message.author.id)
 	await ctx.channel.send("Master would like to do <@" + user + ">'s mother but he also plans to implement the following commands: \n explain, pedro, todo")
 
@@ -230,22 +230,32 @@ async def test(ctx):
 	for server in bot.guilds:
 		for text in server.text_channels:
 				channel.append(text.id)
+	# print("Channel ID: " + str(channel[0]))
 
 @tasks.loop(hours=1.0)
 async def counter():
 	global channel
 	global changed
+	global dictionary
 
 	loc_dt = eastern.localize(datetime.datetime.now())
 	person = setPerson(loc_dt)
 	now = loc_dt.strftime("%H")
+	beginning = "It is "
+	end = "'s day today"
+	print(channel)
+	print("hour " + now)
 
 	if len(channel) !=0:
 		chan = bot.get_channel(channel[0])
-		if now == 9:
-			changed = False
+		if now == "11":
+			print(now)
+			# changed = False
 			print("It's 9am now")
-			await chan.send("It is <@" + dictionary.get(person) + ">'s day today")
+			if person in dictionary.keys():
+				await chan.send(beginning + "<@" + dictionary.get(person) + ">" + end)
+			else:
+				await chan.send(beginning + person + end)
 
 counter.start()
 
